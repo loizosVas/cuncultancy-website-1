d4a16777b30e071ea184e4487964163ddb830bad
Loizos Vasileiou <loizos-95b@hotmail.com>
2021-06-21 21:07:01 +0300

    initial commit to github

---
+++ app/javascript/my_custom_files/js/test.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        alert("jQuery and custom JS file work!");
+    } else {
+        // jQuery is not loaded
+        alert("jQuery and custom JS file doesn't work");
+    }
+}
